ea8d7fff3b53a6ef1354ba761c01ef87d5d7ca00
janstaffa <jstaffa@janstaffa.cz>
2024-12-12 23:06:46 +0100

    Finished the app

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,78 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const fileSelect = document.querySelector('#fileinput');
+const log = document.querySelector('#log');
+const downloadBtn = document.querySelector('#download');
+function printLog(message, isError = false) {
+    if (!log)
+        return;
+    const paragraph = document.createElement('p');
+    if (isError)
+        paragraph.style.color = 'red';
+    paragraph.innerText = message;
+    log.appendChild(paragraph);
+}
+const HAS_HEADER = true;
+const API_KEY = "<klic>";
+const API_PATH = `https://api.mapy.cz/v1/geocode?apikey=${API_KEY}`;
+function getCoords(address) {
+    return new Promise((res, rej) => {
+        fetch(API_PATH + `&query="${address}"&lang=cs&limit=1`)
+            .then((d) => d.json())
+            .then((d) => {
+            var _a, _b;
+            res(((_b = (_a = d === null || d === void 0 ? void 0 : d['items']) === null || _a === void 0 ? void 0 : _a[0]) === null || _b === void 0 ? void 0 : _b['position']) || null);
+        });
+    });
+}
+const createWpt = (coords, name, description) => {
+    const wpt = document.createElement('wpt');
+    wpt.setAttribute('lat', coords.lat.toString());
+    wpt.setAttribute('lon', coords.lon.toString());
+    const nameEl = document.createElement('name');
+    nameEl.innerText = name;
+    const descEl = document.createElement('desc');
+    descEl.innerText = description;
+    wpt.appendChild(nameEl);
+    wpt.appendChild(descEl);
+    return wpt;
+};
+function createGPX() {
+    const boilerplate = `<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+    <gpx version="1.1" creator="Microrisc Map tool" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+  </gpx>`;
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(boilerplate, 'text/xml');
+    return xml;
+}
+let resultGPX = '';
+function handleRows(rows) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (rows.length === 0)
+            return;
+        const gpx = createGPX();
+        const gpxBody = gpx.querySelector('gpx');
+        for (let i = HAS_HEADER ? 1 : 0; i < rows.length; i++) {
+            const row = rows[i];
+            const name = row[0];
+            const address = row[1];
+            const coords = yield getCoords(address);
+            if (!coords) {
+                printLog(`Nepodařilo se získat souřadnice pro: ${name}, ${address}`, true);
+                continue;
+            }
+            const wpt = createWpt(coords, name, address);
+            console.log(wpt);
+            gpxBody.appendChild(wpt);
+            printLog(`Převedeno: ${name}, ${address}`);
+        }
+        resultGPX = new XMLSerializer().serializeToString(gpx);
+        downloadBtn.classList.remove('hidden');
+    });
+}
 fileSelect === null || fileSelect === void 0 ? void 0 : fileSelect.addEventListener('change', (e) => __awaiter(void 0, void 0, void 0, function* () {
+    resultGPX = "";
+    downloadBtn.classList.add('hidden');
     const select = e.target;
     if (!select)
         return;
@@ -25,7 +96,28 @@ fileSelect === null || fileSelect === void 0 ? void 0 : fileSelect.addEventListe
         });
         if (!data)
             return;
-        const parsed = Papa.parse(data);
-        console.log(parsed);
+        try {
+            const parsed = Papa.parse(data);
+            const rows = parsed.data;
+            yield handleRows(rows);
+        }
+        catch (e) {
+            alert(e);
+        }
     }
 }));
+function download(filename, data) {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+downloadBtn.addEventListener('click', () => {
+    if (resultGPX.length === 0)
+        return;
+    const now = new Date();
+    download(`converted_${now.toLocaleDateString().replace('/', '-')}.gpx`, resultGPX);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,90 @@
-
 const fileSelect = document.querySelector('#fileinput');
+const log = document.querySelector('#log');
+const downloadBtn = document.querySelector('#download')!;
 
+function printLog(message: string, isError: boolean = false) {
+	if (!log) return;
+	const paragraph = document.createElement('p');
+	if (isError) paragraph.style.color = 'red';
+	paragraph.innerText = message;
+	log.appendChild(paragraph);
+}
+
+const HAS_HEADER = true;
+const API_KEY = "<klic>";
+const API_PATH = `https://api.mapy.cz/v1/geocode?apikey=${API_KEY}`;
+
+interface Coords {
+	lat: number;
+	lon: number;
+}
+function getCoords(address: string): Promise<Coords | null> {
+	return new Promise((res, rej) => {
+		fetch(API_PATH + `&query="${address}"&lang=cs&limit=1`)
+			.then((d) => d.json())
+			.then((d) => {
+				res(d?.['items']?.[0]?.['position'] || null);
+			});
+	});
+}
+const createWpt = (coords: Coords, name: string, description: string) => {
+	const wpt = document.createElement('wpt');
+	wpt.setAttribute('lat', coords.lat.toString());
+	wpt.setAttribute('lon', coords.lon.toString());
+
+	const nameEl = document.createElement('name');
+	nameEl.innerText = name;
+	const descEl = document.createElement('desc');
+	descEl.innerText = description;
+	wpt.appendChild(nameEl);
+	wpt.appendChild(descEl);
+
+	return wpt;
+};
+
+function createGPX() {
+	const boilerplate = `<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+    <gpx version="1.1" creator="Microrisc Map tool" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">
+  </gpx>`;
+
+	const parser = new DOMParser();
+	const xml = parser.parseFromString(boilerplate, 'text/xml');
+
+	return xml;
+}
+
+let resultGPX = '';
+
+async function handleRows(rows: string[][]) {
+	if (rows.length === 0) return;
+	const gpx = createGPX();
+	const gpxBody = gpx.querySelector('gpx')!;
+
+	for (let i = HAS_HEADER ? 1 : 0; i < rows.length; i++) {
+		const row = rows[i];
+		const name = row[0];
+		const address = row[1];
+		const coords = await getCoords(address);
+		if (!coords) {
+			printLog(
+				`Nepodařilo se získat souřadnice pro: ${name}, ${address}`,
+				true
+			);
+			continue;
+		}
+		const wpt = createWpt(coords, name, address);
+		console.log(wpt);
+		gpxBody.appendChild(wpt);
+		printLog(`Převedeno: ${name}, ${address}`);
+	}
+
+	resultGPX = new XMLSerializer().serializeToString(gpx);
+	downloadBtn.classList.remove('hidden');
+}
 fileSelect?.addEventListener('change', async (e) => {
+  resultGPX = "";
+	downloadBtn.classList.add('hidden');
+
 	const select = e.target as HTMLInputElement;
 	if (!select) return;
 	const files = select.files;
@@ -13,9 +96,36 @@ fileSelect?.addEventListener('change', async (e) => {
 			reader.addEventListener('loadend', () => res(reader.result?.toString()));
 			reader.readAsText(file);
 		});
-    if(!data) return;
+		if (!data) return;
 
-    const parsed = Papa.parse(data);
-		console.log(parsed);
+		try {
+			const parsed = Papa.parse(data);
+			const rows = parsed.data as string[][];
+			await handleRows(rows);
+		} catch (e) {
+			alert(e);
+		}
 	}
+});
+
+function download(filename: string, data: string) {
+	const element = document.createElement('a');
+	element.setAttribute(
+		'href',
+		'data:text/plain;charset=utf-8,' + encodeURIComponent(data)
+	);
+	element.setAttribute('download', filename);
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+}
+
+downloadBtn.addEventListener('click', () => {
+  if(resultGPX.length === 0) return;
+	const now = new Date();
+	download(`converted_${now.toLocaleDateString().replace('/', '-')}.gpx`, resultGPX);
 });
